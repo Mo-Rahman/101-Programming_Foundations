7ebe39aeb405b7db6fd348166e1ff1dc1b589a60
Mohammed Rahman <129rahman@gmail.com>
2017-05-11 13:10:00 +0100

    Examples practice

---
+++ lesson_5_advanced_ruby_collections/working_with_blocks/example_10.rb
@@ -0,0 +1,17 @@
+# Example 10
+
+# Let's say we have the following data structure of nested arrays and we want to increment every number by 1 without changing the data structure.
+
+[[[1, 2], [3, 4]], [5, 6]].map do |arr|
+  arr.map do |el|
+    if el.to_s.size == 1    # it's an integer
+      el + 1
+    else                    # it's an array
+      el.map do |n|
+        n + 1
+      end
+    end
+  end
+end
+
+[[[1, 2], [3, 4]], [5, 6]]
