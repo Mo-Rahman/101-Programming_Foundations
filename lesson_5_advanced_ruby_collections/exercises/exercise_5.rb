348b182b76df248f53ad203afe0262ebb9966237
Mohammed Rahman <129rahman@gmail.com>
2017-05-11 20:06:03 +0100

    Exercises

---
+++ lesson_5_advanced_ruby_collections/exercises/exercise_5.rb
@@ -0,0 +1,23 @@
+# Exercise 5
+
+# Given this nested Hash:
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_age = 0
+
+munsters.each_value do |details|
+  total_male_age += details["age"] if details["gender"] = 'male'
+end
+
+puts total_male_age
+
+# figure out the total age of just the male members of the family.
+
+#.reduce(:+) if name['gender'] == 'male'
